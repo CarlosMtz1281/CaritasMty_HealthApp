039f530b37c3a6cd8d37e7420a865ae6f7170741
Fernando Perez <ferpega03@gmail.com>
2024-08-20 18:36:38 -0600

    test

---
+++ testapp/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Fernando Perez on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
