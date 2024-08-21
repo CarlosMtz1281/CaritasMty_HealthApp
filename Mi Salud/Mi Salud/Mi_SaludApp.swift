07d9a36d189ac0bf1aea8688f163169e46b03539
Fernando Perez <ferpega03@gmail.com>
2024-08-20 18:53:49 -0600

    Created Mi salud app

---
+++ Mi Salud/Mi Salud/Mi_SaludApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mi_SaludApp.swift
+//  Mi Salud
+//
+//  Created by Fernando Perez on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Mi_SaludApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
