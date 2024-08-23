91d96bbd0bd454c8f000a1ab7ce490791841eb5a
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-08-23 10:09:27 -0600

    modified constants

--- Mi Salud/Mi Salud/Consts.swift
+++ Mi Salud/Mi Salud/Consts.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import SwiftUI
 
 struct Constants {
     // MARK: - Colors
     struct Colors {
         static let PANTONE_320_C = UIColor(named: "PANTONE_320_C") ?? UIColor.gray
-        static let PANTONE_302_C = UIColor(named: "PANTONE_302_C") ?? UIColor.gray
-        static let PANTONE_601_C = UIColor(named: "PANTONE_601_C") ?? UIColor.gray
+        static let primary = UIColor(named: "PANTONE_302_C") ?? UIColor.gray
+        static let background = UIColor(named: "PANTONE_601_C") ?? UIColor.gray
         static let PANTONE_COOL_GRAY_8_C = UIColor(named: "PANTONE_COOL_GRAY_8_C") ?? UIColor.gray
         static let PANTONE_621_C = UIColor(named: "PANTONE_621_C") ?? UIColor.gray
-        static let PANTONE_1575_C = UIColor(named: "PANTONE_1575_C") ?? UIColor.gray
+        static let accent = UIColor(named: "PANTONE_1575_C") ?? UIColor.gray
+        static let fontColor = Color(.black)
+        static let fontColor2 = Color(.white)
     }
     
     // MARK: - Endpoints
