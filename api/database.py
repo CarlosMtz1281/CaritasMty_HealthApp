388e7b7af24b75bbc84081c06b7c3fd7fb4d9d18
GermanS04 <german.salasmar@gmail.com>
2024-09-18 10:33:32 -0600

    Fix Library to connect database API

--- api/database.py
+++ api/database.py
@@ -1,22 +1,33 @@
-import mssql_functions as MSSql
+import pyodbc
 import sys
 
-vm_params = {}
-vm_params['DB_HOST'] = '100.80.80.7'
-vm_params['DB_NAME'] = 'alumno02'
-vm_params['DB_USER'] = 'SA'
-vm_params['DB_PASSWORD'] = 'Shakira123.'
+vm_params = {
+    'DB_HOST': '100.80.80.7',
+    'DB_NAME': 'alumno02',
+    'DB_USER': 'SA',
+    'DB_PASSWORD': 'Shakira123.'
+}
 
-local_params = {}
-local_params['DB_HOST'] = "localhost:1433"
-local_params['DB_NAME'] = "master"
-local_params['DB_USER'] = "sa"
-local_params['DB_PASSWORD'] = "5abr1t0n3s_GOAT"
-cnx = None
+local_params = {
+    'DB_HOST': 'localhost',
+    'DB_NAME': 'master',
+    'DB_USER': 'sa',
+    'DB_PASSWORD': '5abr1t0n3s_GOAT'
+}
 
-try:
-    MSSql.cnx = MSSql.mssql_connect(local_params)
-    cnx = MSSql.cnx;
-except Exception as e:
-    print("Cannot connect to mssql server!: {}".format(e))
-    sys.exit()
+def connect_to_db(params):
+    """Conecta a la base de datos utilizando pyodbc y devuelve la conexión."""
+    conn_str = (
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={params['DB_HOST']};"
+        f"DATABASE={params['DB_NAME']};"
+        f"UID={params['DB_USER']};"
+        f"PWD={params['DB_PASSWORD']}"
+    )
+    try:
+        return pyodbc.connect(conn_str)
+    except pyodbc.Error as e:
+        print(f"Cannot connect to MSSQL server: {e}")
+        sys.exit()
+
+cnx = connect_to_db(local_params)
