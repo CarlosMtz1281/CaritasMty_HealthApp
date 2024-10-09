038a474458f56d283060674ec87ee56c2697e6d4
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-10-09 11:44:17 -0600

    loggg hotfix

--- api/logger.py
+++ api/logger.py
@@ -1,8 +1,10 @@
-# logger.py
 import logging
 import logging.handlers
+import os
 
 LOG_PATH = '/var/log/api_http'
+os.makedirs(LOG_PATH, exist_ok=True)  # Ensure the directory exists
+
 LOGFILE = LOG_PATH + '/api_http.log'
 logformat = '%(asctime)s.%(msecs)03d %(levelname)s: %(message)s'
 formatter = logging.Formatter(logformat, datefmt='%d-%b-%y %H:%M:%S')
@@ -10,6 +12,7 @@ formatter = logging.Formatter(logformat, datefmt='%d-%b-%y %H:%M:%S')
 loggingRotativo = False
 DEV = True
 
+# FileHandler for logging to file
 if loggingRotativo:
     LOG_HISTORY_DAYS = 3
     handler = logging.handlers.TimedRotatingFileHandler(
@@ -20,8 +23,14 @@ else:
     handler = logging.FileHandler(filename=LOGFILE)
 
 handler.setFormatter(formatter)
+
+# Create a StreamHandler to log to the console
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
 my_logger = logging.getLogger("api_http")
 my_logger.addHandler(handler)
+my_logger.addHandler(console_handler)  # Add the console handler
 
 if DEV:
     my_logger.setLevel(logging.DEBUG)
